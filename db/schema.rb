523e6332fa1086ed2cd5d5d6c98130e24013987e
Vinicius Marques <v.alvesmarques@gmail.com>
2021-10-16 11:24:05 -0300

    Add bookmark model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_16_141909) do
+ActiveRecord::Schema.define(version: 2021_10_16_142239) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "bookmarks", force: :cascade do |t|
+    t.text "comment"
+    t.integer "movie_id"
+    t.integer "list_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "lists", force: :cascade do |t|
     t.string "name"
